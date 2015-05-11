981026935daa4aadf23e9908b481592321c08cac
GeekZoo <opensource@geek-zoo.com>
2015-05-11 20:55:04 +0800

    no message

--- android/src/com/external/alipay/Constant.java
+++
@@ -1,6 +0,0 @@
-package com.external.alipay;
-
-public class Constant
-{
-	public final static String server_url = "https://msp.alipay.com/x.htm";
-}
--- android/src/com/external/imagezoom/utils/IDisposable.java
+++
@@ -1,6 +0,0 @@
-package com.external.imagezoom.utils;
-
-public interface IDisposable {
-
-	void dispose();
-}
